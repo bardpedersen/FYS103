a526a8cf43f4b1beee8f12f6c89d7e077073113f
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-01-17 15:47:17 +0100

    added all equation

--- Oppgaver/8.22.py
+++ Oppgaver/8.22.py
@@ -5,7 +5,8 @@ height = [1.31, 1.13, 0.89, 0.51, 0.07]  # In meter
 
 sum_x = 0
 sum_x_squared = 0
-sum_x_squared_squared = 0
+sum_x_squared_3 = 0
+sum_x_squared_4 = 0
 sum_xy = 0
 sum_x_squared_y = 0
 sum_y = 0
@@ -13,15 +14,16 @@ n = len(time)
 
 for i, value in enumerate(height):
     sum_x_squared += time[i]**2
-    sum_x_squared_squared += time[i]**4
+    sum_x_squared_3 += time[i] ** 3
+    sum_x_squared_4 += time[i] ** 4
     sum_xy += time[i] * value
     sum_y += value
     sum_x_squared_y += time[i]**2 * value
 
 a, b, c = sympy.symbols('a, b, c')
-eq1 = sympy.Eq(a * n + c * sum_x_squared, sum_y)
-eq2 = sympy.Eq(b * sum_x_squared, sum_xy)
-eq3 = sympy.Eq(a * sum_x_squared + c * sum_x_squared_squared, sum_x_squared_y)
+eq1 = sympy.Eq(a * n + b * sum_x + c * sum_x_squared, sum_y)
+eq2 = sympy.Eq(a * sum_x + b * sum_x_squared + c * sum_x_squared_3, sum_xy)
+eq3 = sympy.Eq(a * sum_x_squared + b * sum_x_squared_3 + c * sum_x_squared_4, sum_x_squared_y)
 result = sympy.solve([eq1, eq2, eq3], (a, b, c))
 
 result_a = float(result[a])
